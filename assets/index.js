f4dd98a2380dbd5fa2af79acc98361f60925fafa
Bellahxz <isabellagrorossimuniz@gmail.com>
2023-07-19 11:51:21 -0300

    add export

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+// Componentes
+export * from './components/logo'
+export * from './components/titulo'
+export * from './components/quadro'
+export * from './components/paragrafo'
+export * from './components/botao'
+export * from './components/bolinhas'
+
+// Seções
+export * from './Sections/hero-section'
+export * from './Sections/a-barbearia-section'
+export * from './Sections/servicos-section'
+export * from './Sections/contatos-section'
+
+// Páginas
+export * from './pages/home-page'
