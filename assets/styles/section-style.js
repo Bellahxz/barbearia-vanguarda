75dd9c60338fa915d61a296b146410632284e789
Bellahxz <isabellagrorossimuniz@gmail.com>
2023-07-19 08:21:37 -0300

    add sections

---
+++ assets/styles/section-style.js
@@ -0,0 +1,20 @@
+import { css } from "lit";
+
+export const section = css`
+  :host {
+    width: 100%;
+    height: 100svh; /*Smallest View Height */
+    
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 4.375rem;
+
+    padding: 2rem;
+    overflow: hidden;
+    position: relative;
+  }
+
+`
